62d6e0d1a4a43fbe5905f76a8f24b570a9138466
Alex Keller <kelleralex04@gmail.com>
2023-12-02 22:07:34 -0700

    requires airport for destination to avoid blank destination

--- models/flight.js
+++ models/flight.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const destinationSchema = new Schema({
     airport: {
         type: String,
-        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
     },
     arrival: Date
 }, {
